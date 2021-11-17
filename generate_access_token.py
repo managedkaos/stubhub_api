55a4971d082ac588b4927a95b2161e02a2e4c1b7
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-11-17 01:39:24 -0800

    Update code for generating an access token

---
+++ generate_access_token.py
@@ -0,0 +1,34 @@
+import os
+import base64
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.getenv('CLIENT_CREDENTIALS_URL')
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+basic_authorization_token = base64.b64encode(
+        bytes(
+            consumer_key + ':' + consumer_secret,
+            encoding="raw_unicode_escape")
+        )
+
+data = json.dumps({
+    "username": username,
+    "password": password
+})
+
+headers = {
+  'Authorization': 'Basic ' + basic_authorization_token.decode('UTF-8'),
+  'Content-Type': 'application/json',
+}
+
+response = requests.request("POST", url, headers=headers, data=data)
+response = json.loads(response.text)
+
+print(response['access_token'])
