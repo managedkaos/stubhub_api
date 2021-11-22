896c7c4433468528287b9a8f7f9079a031d078ec
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-11-22 13:36:23 -0800

    converted ID to int; added venue and ticketInfo; added ingestionDate

--- main.py
+++ main.py
@@ -14,37 +14,37 @@ DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
 REGION_NAME = os.getenv("REGION_NAME")
 
 class Simple(BaseModel):
-    id: Union[UUID, int, str]
+    id: int
     name: str
 
 
 class Venue(BaseModel):
-    id: Union[UUID, int, str]
+    id: int
     name: str
     city: str
     state: str
     postalCode: str
     country: str
-    venueConfigId: Union[UUID, int, str]
+    venueConfigId: int
     venueConfigName: str
     latitude: str
     longitude: str
 
 
 class Ticketinfo(BaseModel):
-    minPrice: Union[str, int]
-    minListPrice: Union[str, int]
-    maxListPrice: Union[str, int]
+    minPrice: Union[int, str]
+    minListPrice: Union[int, str]
+    maxListPrice: Union[int, str]
     totalTickets: int
     totalListings: int
 
 
 class EventId(BaseModel):
-    id: Union[UUID, int, str]
+    id: int
 
 
 class Event(BaseModel):
-    id: Union[UUID, int, str]
+    id: int
     status: str
     locale: str
     name: str
@@ -79,7 +79,7 @@ async def get_event(eventId):
     table = database.Table(DYNAMODB_TABLE)
 
     try:
-        response = table.get_item(Key={'id': eventId})
+        response = table.get_item(Key={'id': int(eventId)})
     except ClientError as e:
         message = "Client error. {}. region_name={}; table_name={}".format(e.response['Error']['Message'], REGION_NAME, DYNAMODB_TABLE)
         print(message)
@@ -100,26 +100,27 @@ async def post_event(eventId):
         response = requests.request("GET", STUBHUB_EVENTS_URL, params=params, headers=headers)
         print(response.text)
     except requests.exceptions.RequestException as e:
-        message="Failed to connect to StubHub API. {}".format(e)
+        message = "Failed to connect to StubHub API. {}".format(e)
         print(message)
         raise HTTPException(status_code=500, detail=message)
     else:
         if "fault" in response.json():
-            message="Stubhub API fault. {}".format(response.json()['fault']['faultstring'])
+            message = "Stubhub API fault. {}".format(response.json()['fault']['faultstring'])
             print(message)
             raise HTTPException(status_code=500, detail=message)
         if "error" in response.json():
-            message="StubHub API error. {}".format(response.json()['error']['message'])
+            message = "StubHub API error. {}".format(response.json()['error']['message'])
             print(message)
             raise HTTPException(status_code=404, detail=message)
         else:
             item = response.json()['events'][0]
+            print(item['ticketInfo'])
 
     try:
         database = boto3.resource('dynamodb', region_name=REGION_NAME)
         table = database.Table(DYNAMODB_TABLE)
         response = table.put_item(Item={
-            "id": str(item['id']),
+            "id": item['id'],
             "status": item['status'],
             "locale": item['locale'],
             "name": item['name'],
@@ -131,13 +132,29 @@ async def post_event(eventId):
             "lastUpdatedDate":  item['lastUpdatedDate'],
             "hideEventDate":  item['hideEventDate'],
             "hideEventTime":  item['hideEventTime'],
-            #"venue": item['venue'],
+            "venue": {
+                "id": item['venue']['id'],
+                "name": item['venue']['name'],
+                "city": item['venue']['city'],
+                "state": item['venue']['state'],
+                "postalCode": item['venue']['postalCode'],
+                "country": item['venue']['country'],
+                "venueConfigId": item['venue']['venueConfigId'],
+                "venueConfigName": item['venue']['venueConfigName'],
+                "latitude": str(item['venue']['latitude']),
+                "longitude": str(item['venue']['longitude'])},
             "timezone":  item['timezone'],
             "currencyCode":  item['currencyCode'],
-            #"ticketInfo": item['ticketInfo'],
+            "ticketInfo": {
+                "minPrice": str(item['ticketInfo']['minPrice']),
+                "minListPrice": str(item['ticketInfo']['minListPrice']),
+                "maxListPrice": str(item['ticketInfo']['maxListPrice']),
+                "totalTickets": str(item['ticketInfo']['totalTickets']),
+                "totalListings": str(item['ticketInfo']['totalListings'])},
             "performers": item['performers'],
             "ancestors": item['ancestors'],
-            "categoriesCollection": item['categoriesCollection']
+            "categoriesCollection": item['categoriesCollection'],
+            "ingestionDate": str(datetime.utcnow().isoformat())
             })
     except ClientError as e:
         message = "DynamoDB client error. {}".format(e.response['Error']['Message'])
